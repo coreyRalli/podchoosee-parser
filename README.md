6b3ec1b1aac309d23bc5f89b5687998ef1760b73
Corey Ralli <coreyralli@shoutingattheinter.net>
2015-04-01 07:04:40 +1100

    Bug fixes, readme update and license

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 ﻿# podchoosee-parser
+A simple utility for parsing podcast feeds for node js.
+
+## Usage
+
+Using podchoosee-parser is super easy: 
+
+#####With promses (with thanks to the awesome Q library)
+```
+var pp = require('podchoosee-parser');
+pp.getSubscriptionPromise('url-of-feed-here')
+.done(function(response) {
+	console.log(response.subscription.title);
+});
+```
+
+#####Or with callbacks... if you like things old-school:
+```
+var pp = require('podchoosee-parser');
+pp.getSubscription('url-of-feed-here', function (err, response) {
+	if (err) {
+		console.log("Something went wrong!");
+	} else {
+		console.log(response.subscription.title);
+	}
+});
+```
+
+This is what a typical response looks like (in JSON):
+
+```
+{
+   "subscription":{
+      "title":"Hollywood Handbook",
+      "websiteUrl":"http://www.earwolf.com/show/hollywood-handbook/",
+      "description":"Hollywood Handbook is an insider's guide to achieving your showbiz dreams from two A-List it-boys who are living theirs. Hayes and Sean provide an exclusive VIP backstage pass into Tinseltown politics, answer questions from unsuccessful listeners, and bring in famous guests to discuss their craft and how they became what they are (famous).",
+      "copyright":"Earwolf Media, LLC",
+      "author":"Earwolf",
+      "category":"Comedy",
+      "iTunesImageUrl":"http://cdn.earwolf.com/wp-content/uploads/2013/09/HollywoodHandbook_1600x1600_Cover1.jpg",
+      "isExplict":true,
+      "summary":"Hollywood Handbook is an insider's guide to achieving your showbiz dreams from two A-List it-boys who are living theirs. Hayes and Sean provide an exclusive VIP backstage pass into Tinseltown politics, answer questions from unsuccessful listeners, and bring in famous guests to discuss their craft and how they became what they are (famous).",
+      "subtitle":"",
+      "imageUrl":"http://cdn.earwolf.com/wp-content/uploads/2013/09/HollywoodHandbook_1600x1600_Cover1.jpg"
+   },
+   "episodes":[
+      {
+         "title":"The ScuzzMan, Dom's Close Friend",
+         "episodeWebLink":"http://www.earwolf.com/episode/the-scuzzman-doms-close-friend/",
+         "description":"With Hayes gone, Sean brings in his assistant Dom to fill the void and showers him with apologies in exchange for the exciting golden-hairedguest he booked. Then, Dom's friend The ScuzzMan joins them to discuss Volleyball: A Real Underdog Story. Fina...",
+         "rssGUID":"http://www.earwolf.com/episode/the-scuzzman-doms-close-friend/",
+         "mediaThumbnail":"http://cdn.earwolf.com/wp-content/uploads/2013/09/HollywoodHandbook_1600x1600_Cover1.jpg",
+         "pubDate":"2015-03-31T07:02:04.000Z",
+         "isExplicit":true,
+         "subtitle":"With Hayes gone, Sean brings in his assistant Dom to fill the void and showers him with apologies in exchange for the exciting golden-hairedguest he booked. Then, Dom's friend The ScuzzMan joins them to discuss Volleyball: A Real Underdog Story. Fina...",
+         "summary":"With Hayes gone, Sean brings in his assistant Dom to fill the void and showers him with apologies in exchange for the exciting golden-hairedguest he booked. Then, Dom's friend The ScuzzMan joins them to discuss Volleyball: A Real Underdog Story. Finally, the Popcorn Gallery is back to ask some Goldie Hawn-themed questions.  ",
+         "mediaType":0,
+         "mediaFileUrl":"http://feeds.soundcloud.com/stream/198524608-hollywoodhandbook-the-scuzzman-doms-close-friend.mp3",
+         "mediaFileSize":"53486554"
+      }
+      ...
+   ]
+}
+
+```
+
+Most of it is self-explanitory except for a couple of things:
+
+1. ```subtitle``` vs ```summary``` vs ```description```: The ```subtitle``` & ```summary``` properties come from the RSS feed's iTunes tags. ```description``` tends to be a raw (HTML tags and such) output, while the itunes tags are pre-formatted. The ```summary`` tag provides a full description and ```subtitle``` only a single sentence or nothing at all.
+
+2. ```mediaType```: The ```mediaType``` property in the episode object is a interger, with 0 being audio, 1 video and 2 anything else.
 
 
+## Dependencies
+Podchoosee-Parser uses the following packages:
+
+[Q](https://github.com/kriskowal/q)
+[xml-node-lite](https://github.com/hgourvest/node-xml-lite)
+[request](https://github.com/request/request)
--- app.js
+++ app.js
@@ -6,298 +6,224 @@ var PodchooseeParser = function () {
 
 };
 
-PodchooseeParser.prototype = (function () {
-    function getXML(uri) {
-        var deferral = Q.defer();
-        
-        var requestPromiseWrapper = Q.denodeify(Request);
-        
-        requestPromiseWrapper(uri, { headers: { "User-Agent" : PodchooseeParser.userAgent } })
+PodchooseeParser.userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0";
+PodchooseeParser.getXML = function (uri) {
+    var deferral = Q.defer();
+    
+    var requestPromiseWrapper = Q.denodeify(Request);
+    
+    requestPromiseWrapper(uri, { headers: { "User-Agent" : PodchooseeParser.userAgent } })
         .done(function (response) {
-            var xml = NodeXMLLite.parseString(response[0].body);
-            
-            var channelNode = xml.childs[0].childs;
-            
-            deferral.resolve(channelNode);
-        }, function (ex) {
-            deferral.reject(ex);
-        });
+        var xml = NodeXMLLite.parseString(response[0].body);
         
-        return deferral.promise;
+        var channelNode = xml.childs[0].childs;
+        
+        deferral.resolve(channelNode);
+    }, function (ex) {
+        deferral.reject(ex);
+    });
+    
+    return deferral.promise;
+}
+PodchooseeParser.parseSubscription = function (xml) {
+    var deferral = Q.defer();
+    
+    var subscription = {};
+    
+    try {
+        for (var n = 0; n < xml.length; n++) {
+            if (xml[n].name == "item") {
+                break;
+            } else {
+                switch (xml[n].name) {
+                    case "title":
+                        subscription.title = xml[n].childs[0];
+                        break;
+                    case "link":
+                        subscription.websiteUrl = xml[n].childs[0];
+                        break;
+                    case "copyright":
+                        subscription.copyright = xml[n].childs[0];
+                        break;
+                    case "itunes:author":
+                        subscription.author = xml[n].childs[0];
+                        break;
+                    case "description":
+                        subscription.description = xml[n].childs[0];
+                        break;
+                    case "itunes:summary":
+                        subscription.summary = xml[n].childs[0];
+                        break;
+                    case "itunes:subtitle":
+                        subscription.subtitle = xml[n].childs[0];
+                        break;
+                    case "itunes:category":
+                        subscription.category = xml[n].attrib.text;
+                        break;
+                    case "image":
+                        subscription.imageUrl = xml[n].childs[0].childs[0];
+                        break;
+                    case "itunes:image":
+                        subscription.iTunesImageUrl = xml[n].attrib.href;
+                        break;
+                    case "itunes:explicit":
+                        subscription.isExplict = (xml[n].childs[0] == "Yes") ? true : false;
+                        break;
+                    case "media:thumbnail":
+                        subscription.mediaThumbnail = xml[n].attrib.url;
+                        break;
+                }
+            }
+        }
+        
+        deferral.resolve(subscription);
+    }
+        catch (e) {
+        deferral.reject(e);
     }
     
-    function parseSubscription(xml) {
-        var deferral = Q.defer();
-        
-        var subscription = {};
-        
-        try {
-            for (var n = 0; n < xml.length; n++) {
-                if (xml[n].name == "item") {
-                    break;
-                } else {
-                    switch (xml[n].name) {
+    return deferral.promise;
+}
+PodchooseeParser.parseEpisodes = function (xml) {
+    var deferral = Q.defer();
+    
+    var episodes = [];
+    
+    try {
+        for (var n = 0; n < xml.length; n++) {
+            var episode = {};
+            
+            if (xml[n].name == "item") {
+                var childNodes = xml[n].childs
+                
+                for (var i = 0; i < childNodes.length; i++) {
+                    switch (childNodes[i].name) {
                         case "title":
-                            subscription.title = xml[n].childs[0];
+                            episode.title = childNodes[i].childs[0];
                             break;
                         case "link":
-                            subscription.websiteUrl = xml[n].childs[0];
-                            break;
-                        case "copyright":
-                            subscription.copyright = xml[n].childs[0];
-                            break;
-                        case "itunes:author":
-                            subscription.author = xml[n].childs[0];
+                            episode.episodeWebLink = childNodes[i].childs[0];
                             break;
                         case "description":
-                            subscription.description = xml[n].childs[0];
+                            episode.description = childNodes[i].childs[0];
                             break;
                         case "itunes:summary":
-                            subscription.summary = xml[n].childs[0];
+                            episode.summary = childNodes[i].childs[0];
                             break;
                         case "itunes:subtitle":
-                            subscription.subtitle = xml[n].childs[0];
+                            episode.subtitle = childNodes[i].childs[0];
                             break;
-                        case "itunes:category":
-                            subscription.category = xml[n].attrib.text;
+                        case "guid":
+                            episode.rssGUID = childNodes[i].childs[0];
                             break;
-                        case "image":
-                            subscription.imageUrl = xml[n].childs[0].childs[0];
+                        case "enclosure":
+                            var type = childNodes[i].attrib.type;
+                            
+                            if (type.lastIndexOf("audio", 0) === 0) { episode.mediaType = 0; }
+                            else if (type.lastIndexOf("video", 0) === 0) { episode.mediaType = 1; }
+                            else { episode.mediaType = 2; }
+                            
+                            episode.mediaFileUrl = childNodes[i].attrib.url;
+                            
+                            episode.mediaFileSize = childNodes[i].attrib.length;
+                            
                             break;
-                        case "itunes:image":
-                            subscription.iTunesImageUrl = xml[n].attrib.href;
+
+                        case "pubDate":
+                            var d = new Date(childNodes[i].childs[0]);
+                            
+                            var convertedString = d.toISOString();
+                            
+                            episode.pubDate = convertedString;
+                            
                             break;
-                        case "itunes:explicit":
-                            subscription.isExplict = (xml[n].childs[0] == "Yes") ? true : false;
-                            break;
+
                         case "media:thumbnail":
-                            subscription.mediaThumbnail = xml[n].attrib.url;
+                            episode.mediaThumbnail = childNodes[i].attrib.url;
+                            break;
+
+                        case "itunes:explicit":
+                            episode.isExplicit = (childNodes[i].childs[0] == "Yes") ? true : false;
+                            break;
+
+                        case "image":
+                            episode.episodeImageUrl = childNodes[i].childs[0].childs[0];
                             break;
                     }
                 }
-            }
-            
-            deferral.resolve(subscription);
-        }
-        catch (e) {
-            deferral.reject(e);
-        }
-        
-        return deferral.promise;
-    }
-    
-    function parseEpisode(xml) {
-        var deferral = Q.defer();
-        
-        var episodes = [];
-        
-        try {
-            for (var n = 0; n < xml.length; n++) {
-                var episode = {};
                 
-                if (xml[n].name == "item") {
-                    var childNodes = xml[n].childs
-                    
-                    for (var i = 0; i < childNodes.length; i++) {
-                        switch (childNodes[i].name) {
-                            case "title":
-                                episode.title = childNodes[i].childs[0];
-                                break;
-                            case "link":
-                                episode.episodeWebLink = childNodes[i].childs[0];
-                                break;
-                            case "description":
-                                episode.description = childNodes[i].childs[0];
-                                break;
-                            case "itunes:summary":
-                                episode.summary = childNodes[i].childs[0];
-                                break;
-                            case "itunes:subtitle":
-                                episode.subtitle = childNodes[i].childs[0];
-                                break;
-                            case "guid":
-                                episode.rssGUID = childNodes[i].childs[0];
-                                break;
-                            case "enclosure":
-                                var type = childNodes[i].attrib.type;
-                                
-                                if (type.lastIndexOf("audio", 0) === 0) { episode.mediaType = 0; }
-                                else if (type.lastIndexOf("video", 0) === 0) { episode.mediaType = 1; }
-                                else { episode.mediaType = 2; }
-                                
-                                episode.mediaFileUrl = childNodes[i].attrib.url;
-                                
-                                episode.mediaFileSize = childNodes[i].attrib.length;
-                                
-                                break;
-
-                            case "pubDate":
-                                var d = new Date(childNodes[i].childs[0]);
-                                
-                                var convertedString = d.toISOString();
-                                
-                                episode.pubDate = convertedString;
-                                
-                                break;
-
-                            case "media:thumbnail":
-                                episode.mediaThumbnail = childNodes[i].attrib.url;
-                                break;
-
-                            case "itunes:explicit":
-                                episode.isExplicit = (childNodes[i].childs[0] == "Yes") ? true : false;
-                                break;
-
-                            case "image":
-                                episode.episodeImageUrl = childNodes[i].childs[0].childs[0];
-                                break;
-                        }
-                    }
-                    
-                    episodes.push(episode);
-                }
+                episodes.push(episode);
             }
-            
-            deferral.resolve(episodes);
-
         }
+        
+        deferral.resolve(episodes);
+
+    }
     catch (e) {
-            deferral.reject(e);
-        }
-        var deferral = Q.defer();
-        
-        var episodes = [];
-        
-        try {
-            for (var n = 0; n < xml.length; n++) {
-                var episode = new Episode();
-                
-                if (xml[n].name == "item") {
-                    var childNodes = xml[n].childs
-                    
-                    for (var i = 0; i < childNodes.length; i++) {
-                        switch (childNodes[i].name) {
-                            case "title":
-                                episode.title = childNodes[i].childs[0];
-                                break;
-                            case "link":
-                                episode.episodeWebLink = childNodes[i].childs[0];
-                                break;
-                            case "description":
-                                episode.description = childNodes[i].childs[0];
-                                break;
-                            case "itunes:summary":
-                                episode.summary = childNodes[i].childs[0];
-                                break;
-                            case "itunes:subtitle":
-                                episode.subtitle = childNodes[i].childs[0];
-                                break;
-                            case "guid":
-                                episode.rssGUID = childNodes[i].childs[0];
-                                break;
-                            case "enclosure":
-                                var type = childNodes[i].attrib.type;
-                                
-                                if (type.lastIndexOf("audio", 0) === 0) { episode.mediaType = 0; }
-                                else if (type.lastIndexOf("video", 0) === 0) { episode.mediaType = 1; }
-                                else { episode.mediaType = 2; }
-                                
-                                episode.mediaFileUrl = childNodes[i].attrib.url;
-                                
-                                episode.mediaFileSize = childNodes[i].attrib.length;
-                                
-                                break;
-
-                            case "pubDate":
-                                var d = new Date(childNodes[i].childs[0]);
-                                
-                                var convertedString = d.toISOString();
-                                
-                                episode.pubDate = convertedString;
-                                
-                                break;
-
-                            case "media:thumbnail":
-                                episode.mediaThumbnail = childNodes[i].attrib.url;
-                                break;
-
-                            case "itunes:explicit":
-                                episode.isExplicit = (childNodes[i].childs[0] == "Yes") ? true : false;
-                                break;
-
-                            case "image":
-                                episode.episodeImageUrl = childNodes[i].childs[0].childs[0];
-                                break;
-                        }
-                    }
-                    
-                    episodes.push(episode);
-                }
-            }
-            
-            deferral.resolve(episodes);
-
-        }
-        catch (e) {
-            deferral.reject(e);
-        }
+        deferral.reject(e);
     }
     
-    return ({
-        getSubscription: function (url, callback) {
-            var subContainer = {
-                subscription: null,
-                episodes: []
-            };
+    return deferral.promise;
+}
 
-            getXML(url)
-            .then(function (xml) {
-                return parseSubscription(xml);
-            })
-            .then(function (sub) {
-                subContainer.subscription = sub;
-                
-                return parseEpisode(xml);
-            })
-            .done(function (eps) {
-                subContainer.episodes = eps;
-
-                callback(null, subContainer);
-            }, function (ex) {
-                callback(ex);
-            });
-        },
-        getSubscriptionPromise: function (url) {
-            var deferral = Q.defer();
-            
-            var subContainer = {
-                subscription: null,
-                episodes: []
-            };
-            
-            getXML(url)
-            .then(function (xml) {
-                return parseSubscription(xml);
-            })
-            .then(function (sub) {
-                subContainer.subscription = sub;
-                
-                return parseEpisode(xml);
-            })
-            .done(function (eps) {
-                subContainer.episodes = eps;
-                
-                deferral.resolve(subContainer);
-            }, function (ex) {
-                deferral.reject(ex);
-            });
-
-            return deferral.promise;
-        }
+PodchooseeParser.getSubscription = function (url, callback) {
+    var subContainer = {
+        subscription: null,
+        episodes: []
+    };
+    
+    var subXML;
+    
+    PodchooseeParser.getXML(url)
+    .then(function (xml) {
+        subXML = xml;
+        return PodchooseeParser.parseSubscription(subXML);
+    })
+    .then(function (sub) {
+        subContainer.subscription = sub;
+        
+        return PodchooseeParser.parseEpisodes(subXML);
+    })
+    .done(function (eps) {
+        subContainer.episodes = eps;
+        
+        callback(null, subContainer);
+    }, function (ex) {
+        callback(ex);
     });
-});
+}
+PodchooseeParser.getSubscriptionPromise = function (url) {
+    var deferral = Q.defer();
+    
+    var subContainer = {
+        subscription: null,
+        episodes: []
+    };
+    
+    var subXML;
+    
+    PodchooseeParser.getXML(url)
+    .then(function (xml) {
+        subXML = xml;
 
-PodchooseeParser.userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0";
+        return PodchooseeParser.parseSubscription(subXML);
+    })
+    .then(function (sub) {
+        subContainer.subscription = sub;
+        
+        return PodchooseeParser.parseEpisodes(subXML);
+    })
+    .done(function (eps) {
+    subContainer.episodes = eps;
+        
+    deferral.resolve(subContainer);
+    }, function (ex) {
+        deferral.reject(ex);
+    });
+    
+    return deferral.promise;
+}
+
 
 module.exports = PodchooseeParser;
 
